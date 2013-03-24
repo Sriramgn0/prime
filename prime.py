d43432a6fcbe733ab48e8580401b607ef9f8d5c8
ashok1230 <gadiashok0410@gmail.com>
2013-03-24 22:24:07 +0530

    first commit

---
+++ prime.py
@@ -0,0 +1,11 @@
+class prime:
+   def __init__(self):
+	number=int(raw_input("enter number"))
+	flag='prime number'
+	for i in range(2,number/2+1):
+	    if number%i==0:
+		flag='not prime number'
+	
+	print flag
+
+a=prime()
