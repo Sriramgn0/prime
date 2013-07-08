2901f9350e9f9a423a8eac3b7fc9a6684e18d8ae
ashok1230 <gadiashok0410@gmail.com>
2013-07-08 09:50:44 +0530

    Update prime.py

--- prime.py
+++ prime.py
@@ -58,3 +58,92 @@ network_subscribe_id = 'appcenter-364425364'
 
 #PUBLISHER
 network_pub_port = '6001'
+
+
+runservers
+
+
+import os, sys
+from multiprocessing import Process
+#APPCENTER PATH
+appcenter= "/home/dev/Documents/Workspace/Appcenter/manage.py"
+
+#BLOG PATH
+emo2 = '/home/dev/Documents/Workspace/Emotion/manage.py'
+
+#OPENID PATH
+openid = "/home/dev/Documents/Workspace/Openid/open/manage.py"
+
+#OAUTH PLAYGROUND PATH
+#oauth = "/home/dev/Documents/Workspace/Oauth/mysite/manage.py"
+
+#OAUTH PATH
+oauth2 = "/home/dev/Documents/Workspace/Oauth2/manage.py"
+
+#ADSPACE PATH
+adspace= "/home/dev/Documents/Workspace/Adspace/manage.py"
+
+#CHANNEL PATH
+channel = "/home/dev/Documents/Workspace/Channel/manage.py"
+
+#NETWORK PATH
+network = "/home/dev/Documents/Workspace/Network/network/manage.py"
+
+#Marketplace PATH
+market = "/home/dev/Documents/Workspace/Marketplace/manage.py"
+
+#Notification PATH
+notification = "/home/dev/Documents/Workspace/Notification/manage.py"
+
+app_data = "/home/dev/Documents/Workspace/appdata/manage.py"
+
+def iters(x):
+	path = x.split('/')[:-1]
+	manage = x.split('/')[-1]
+	dirpath = '/'.join(path)+"/"
+	os.chdir(dirpath)
+	os.system("python "+manage+" &")
+
+def loader(path, command):
+	os.chdir(path)
+	os.system(command)
+
+def starter():
+	try:
+		os.system("ps -ef | grep manage.py | awk '{print $2}' | xargs kill -9")
+	except:
+		pass
+	count = -1
+	while True:		
+		data = raw_input("Do you want to restart the server{y/n}")
+		if data == "y":
+			count = count + 1
+		elif data == "n":
+			p.terminate()
+			os.system("ps -ef | grep manage.py | awk '{print $2}' | xargs kill -9")
+			sys.exit()
+		else:
+			print "\n ONLY {y or n} is allowed \n" 
+			continue
+
+		if count >= 1:
+			print "Terminating process"
+			p.terminate()
+			os.system("ps -ef | grep manage.py | awk '{print $2}' | xargs kill -9")
+		else:
+			plist = [openid, adspace, channel, network, market, oauth2, appcenter, emo2, notification,app_data]
+		for x in plist:
+			try:
+				p = Process(target=iters, args=(x, ))
+				p.start()
+				p.join()
+
+			except KeyboardInterrupt:
+				p.terminate()
+				print "Process End by user"
+			except:
+				print "%s failed" %x
+
+
+if __name__ == "__main__":
+	starter()
