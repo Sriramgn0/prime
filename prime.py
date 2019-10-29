fede9b7e0721874922490bf06bf06a7fc454aa45
sriram gn <38837878+Sriramgn0@users.noreply.github.com>
2019-10-29 18:43:14 +0530

    Update prime.py
    time complexity decreased from O(n) to O(1)

--- prime.py
+++ prime.py
@@ -1,10 +1,9 @@
 class prime:
    def __init__(self):
 	number=int(raw_input("enter number"))
-	flag='prime number'
-	for i in range(2,number/2+1):
-	    if number%i==0:
-		flag='not prime number'
+	flag='not prime number'
+	    if (number==2)||(number==3)||((num+1)%6==0)||((num-1)%6==0)):
+		flag='prime number'
 	
 	print flag
 
